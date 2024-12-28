316976ec655c8291581843d1c2f841c1d17ab866
Avanishyadav9035 <132216852+Avanishyadav9035@users.noreply.github.com>
2024-12-28 15:57:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
+import React, {useState, useEffect } from "react";
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+   const[seconds, setSeconds] = useState(0);
+    useEffect(()=>{
+      const interval = setInterval(()=>{
+        setSeconds((prevSeconds)=> prevSeconds + 1);
+      }, 1000);
+
+      //cleanup interval on component unmount
+      return ()=> clearInterval(interval);
+    }, []);
   return (
-    <div>
+    <div style={{ fontFamily:"Arial, sans-serif", textAlign: "center", marginTop:"20px"}}>
         {/* Do not remove the main div */}
+     <h1>You&apos;ve been on this page for {seconds} seconds.</h1>
     </div>
-  )
-}
+  );
+};
 
 export default App
