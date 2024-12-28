f89548e06473a7cc532c2517e1a839c95e1aef7e
Avanishyadav9035 <132216852+Avanishyadav9035@users.noreply.github.com>
2024-12-28 15:38:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
+const App =() => (
+  <div>
+    <App/>
+  </div>
+);
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
